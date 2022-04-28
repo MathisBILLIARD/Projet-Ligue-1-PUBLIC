57ad86e7fa5cb1132214b42861c5f5933609162d
Roméo Vandoolaeghe <90157318+RomeoVandoolaeghe@users.noreply.github.com>
2022-04-29 00:32:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,29 @@
     <head>
         <title>index</title>
         <meta charset="utf-8" />
+        <link href="index.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
     </head>
 
     <body>
+     
+        <!-- image (logo) -->
+        <img class="logo" src="bidoobet.png" alt="">
 
-        <a href="inscription.php">Inscription</a>
+        <!-- texte de bienvenue -->
+        <div class="bloc_bienvenue">
+            <h1>Bienvenue</h1>
 
-        <a href="connexion_form.php">Connexion</a>
+        </div>
+
+        <!-- bloc avec boutons de connexion et d'inscription -->
+        <div class="bloc">
+            <a href="inscription.php" class="signin">Inscription</a>
+            <a href="connexion_form.php" class="login">Connexion</a>
+        </div>
+
 
     </body>
 </html>
