eec7c4e444f584dcef31742f9cc4eb3a4a59a75e
MathisBILLIARD <90194706+MathisBILLIARD@users.noreply.github.com>
2022-05-08 12:05:34 +0200

    Add files via upload

--- bidoobet.sql
+++ bidoobet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 26, 2022 at 07:51 AM
+-- Generation Time: May 08, 2022 at 09:06 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -36,8 +36,17 @@ CREATE TABLE `matchs` (
   `CoteNul` float NOT NULL,
   `ResultatFin` int(11) DEFAULT NULL,
   `Date` date NOT NULL,
-  `Heure` time NOT NULL
+  `Heure` time NOT NULL,
+  `Championnat` varchar(50) NOT NULL COMMENT '                                       '
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `matchs`
+--
+
+INSERT INTO `matchs` (`ID`, `EquipeD`, `EquipeE`, `CoteD`, `CoteE`, `CoteNul`, `ResultatFin`, `Date`, `Heure`, `Championnat`) VALUES
+(8, 'Liverpool', 'Tottenham', 1.42, 4.9, 7, NULL, '2022-05-07', '20:45:00', 'Premiere League'),
+(9, 'Angers', 'Bordeaux', 2.13, 3.38, 3.48, NULL, '2022-05-08', '15:00:00', 'Ligue 1');
 
 -- --------------------------------------------------------
 
@@ -66,15 +75,18 @@ CREATE TABLE `utilisateurs` (
   `Psswd` varchar(30) NOT NULL,
   `Solde` float NOT NULL,
   `NumTel` varchar(14) NOT NULL,
-  `Role` varchar(10) NOT NULL
+  `Rôle` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`ID`, `Nom`, `Prenom`, `Date`, `Email`, `Psswd`, `Solde`, `NumTel`, `Role`) VALUES
-(1, 'Billiard', 'Mathis', '2003-06-06', 'mathis.billiard139@gmail.com', '25ansPokeInvest', 0, '07 82 81 52 08', 'Admin');
+INSERT INTO `utilisateurs` (`ID`, `Nom`, `Prenom`, `Date`, `Email`, `Psswd`, `Solde`, `NumTel`, `Rôle`) VALUES
+(1, 'Billiard', 'Mathis', '2003-06-06', 'mathis.billiard139@gmail.com', '25ansPokeInvest', 0, '07 82 81 52 08', 'Admin'),
+(7, 'Franck', 'Lin', '1990-07-11', 'francklin@gmail.com', 'Coucou123', 0, '06 26 18 94 32', 'Client'),
+(8, 'Franck', 'Lin', '2003-02-21', 'francklin@gmail.com', 'Coucou123', 10, '07 21 49 64 21', 'Client'),
+(9, 'Vincent', 'Lin', '2003-02-21', 'francklin@gmail.com', 'Coucou123', 0, '07 21 49 64 21', 'Client');
 
 --
 -- Indexes for dumped tables
@@ -107,13 +119,13 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT for table `matchs`
 --
 ALTER TABLE `matchs`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
